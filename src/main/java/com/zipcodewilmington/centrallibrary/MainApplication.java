6151d3a0a32fe682a05045c3add113709cc922dc
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2025-11-10 17:09:48 -0500

    initial commit for library item

--- src/main/java/com/zipcodewilmington/centrallibrary/MainApplication.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/MainApplication.java
@@ -1,8 +1,12 @@
-package com.zipcodewilmington.centralibrary;
+package com.zipcodewilmington.centrallibrary;
 
 /**
  * Created by n3pjk on 6/9/2025.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Central Library");
+    }
 
 }
