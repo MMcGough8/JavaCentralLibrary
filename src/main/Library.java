540a92c0893e9825f47c5d590048ef75e45535ba
mmcgough8 <mwm9490@icloud.com>
2025-11-10 13:58:13 -0500

    "Created empty class with just a name field and constructor"

---
+++ src/main/Library.java
@@ -0,0 +1,11 @@
+public class Library {
+    private String name;
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
