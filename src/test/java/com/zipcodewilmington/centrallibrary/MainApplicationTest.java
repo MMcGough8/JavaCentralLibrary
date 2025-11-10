63459713b4617c63c17cbf45712ccb91831de1bb
deveshmur <dmdevesh@gmail.com>
2025-11-10 12:14:26 -0500

    finished searchable class and fixed pom.xml

--- src/test/java/com/zipcodewilmington/centrallibrary/MainApplicationTest.java
+++ src/test/java/com/zipcodewilmington/centrallibrary/MainApplicationTest.java
@@ -1,7 +1,17 @@
-package com.zipcodewilmington.centralibrary;
+package com.zipcodewilmington.centrallibrary;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by n3pjk on 6/9/2025.
  */
 public class MainApplicationTest {
+
+    MainApplication app = new MainApplication();
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
 }
