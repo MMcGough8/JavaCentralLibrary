985a0cb1117476fd95635cc68b9246fa1863d8b1
deveshmur <dmdevesh@gmail.com>
2025-11-14 10:15:31 -0500

    updated spacing

--- src/main/java/com/zipcodewilmington/centrallibrary/ReadJSON.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/ReadJSON.java
@@ -16,14 +16,12 @@ public class ReadJSON {
             JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("file.json"));
 
 
-
             String libraryName = (String) jsonObject.get("libraryName");
             String address = (String) jsonObject.get("address");
 
             System.out.println("Library Name: " + libraryName);
             System.out.println("Address: " + address);
             System.out.println();
-
 
 
             System.out.println("=== LIBRARIANS ===");
