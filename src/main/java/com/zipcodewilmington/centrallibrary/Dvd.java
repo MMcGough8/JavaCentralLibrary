af2a6a97408698ca00502127d237e6ab8053d4bc
deveshmur <dmdevesh@gmail.com>
2025-11-16 16:15:43 -0500

    added parsing and cleaned up dvd

--- src/main/java/com/zipcodewilmington/centrallibrary/Dvd.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/Dvd.java
@@ -3,51 +3,65 @@ package com.zipcodewilmington.centrallibrary;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Dvd class represents a DVD item in the library.
- * It extends LibraryItem and implements Reservable interface.
- */
+
 public class Dvd extends LibraryItem implements Reservable {
     private String director;
-    private long duration; // in minutes
+    private long duration;
     private String rating;
     private String genre;
+    
     private boolean isReserved;
+    private LibraryMember reservedBy;
 
-    /**
-     * Constructor for Dvd
-     */
     public Dvd(String id, String title, String location, String director, long duration, String rating, String genre) {
         super(id, title, location);
         this.director = director;
         this.duration = duration;
         this.rating = rating;
         this.genre = genre;
+
         this.isReserved = false;
+        this.reservedBy = null;
     }
 
-    // Getters
+
     public String getDirector() {
-        return this.director;
+        return director;
     }
 
     public long getDuration() {
-        return this.duration;
+        return duration;
     }
 
     public String getRating() {
-        return this.rating;
+        return rating;
     }
 
     public String getGenre() {
-        return this.genre;
+        return genre;
     }
 
+    @Override
     public boolean isReserved() {
-        return this.isReserved;
+        return isReserved;
     }
 
-    // Setters
+    @Override
+    public double calculateLateFee(int daysLate) {
+        return daysLate * 1.00; 
+    }
+
+    @Override
+    public int getMaxBorrowDays() {
+        return 7; 
+    }
+
+    @Override
+    public String getItemType() {
+        return "DVD";
+    }
+
+
     public void setDirector(String director) {
         this.director = director;
     }
@@ -64,43 +78,39 @@ public class Dvd extends LibraryItem implements Reservable {
         this.genre = genre;
     }
 
-    @Override
-    public double calculateLateFee(int daysLate) {
-        return daysLate * 1.00; // $1.00 per day
-    }
-
-    @Override
-    public int getMaxBorrowDays() {
-        return 7; // Can be borrowed for 7 days
-    }
-
-    @Override
-    public String getItemType() {
-        return "DVD";
-    }
-
+    
     @Override
     public List<String> getSearchableFields() {
         List<String> fields = new ArrayList<>();
-        fields.add(this.title);
-        fields.add(this.director);
-        fields.add(this.genre);
+        fields.add(getTitle());
+        fields.add(director);
+        fields.add(genre);
+        fields.add(rating);
         return fields;
     }
 
-    @Override
+     @Override
     public void reserve(LibraryMember member) {
-        if (!this.isReserved) {
-            this.isReserved = true;
+        if (isReserved) {
+            throw new Error("DVD is already reserved.");
         }
+        this.isReserved = true;
+        this.reservedBy = member;
     }
 
     @Override
     public void cancelReserve(LibraryMember member) {
+        if (!isReserved) {
+            throw new Error("DVD is not currently reserved.");
+        }
+        if (!reservedBy.equals(member)) {
+            throw new Error("This DVD was reserved by another member.");
+        }
         this.isReserved = false;
+        this.reservedBy = null;
     }
 
-    @Override
+     @Override
     public String toString() {
         return "Dvd{" +
                 "id='" + getId() + '\'' +
