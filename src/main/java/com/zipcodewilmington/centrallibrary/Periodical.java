f797a1703272a4a6a9f95291458dc1fbe596b869
deveshmur <dmdevesh@gmail.com>
2025-11-16 16:18:24 -0500

    added persing and cleaned up periodical

--- src/main/java/com/zipcodewilmington/centrallibrary/Periodical.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/Periodical.java
@@ -3,43 +3,52 @@ package com.zipcodewilmington.centrallibrary;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Periodical extends LibraryItem implements Reservable{
-    
-   
+public class Periodical extends LibraryItem implements Reservable {
+
     private String issueDate;
     private String publisher;
     private String issn;
-    private int volume; 
-	private int issueNumber; 
-	private String publicationDate; 
-    boolean isReserved;
-    
+    private int volume;
+    private int issueNumber;
+    private String publicationDate;
+
+    private boolean isReserved;
+    private LibraryMember reservedBy;
+
  
-    public Periodical(String id, String title, String location, String issueDate, String issn, int volume, int issueNumber, String publisher, String publicationDate) {
+    public Periodical(String id, String title, String location, String issueDate, String issn,
+                      int volume, int issueNumber, String publisher, String publicationDate) {
+
         super(id, title, location);
+
         this.issueDate = issueDate;
         this.publisher = publisher;
         this.issn = issn;
+        this.volume = volume;
         this.issueNumber = issueNumber;
         this.publicationDate = publicationDate;
-        this.volume = volume;
+
+        this.isReserved = false;
+        this.reservedBy = null;
     }
-    
+
+
     @Override
     public String getItemType() {
         return "Periodical";
     }
-    
+
     @Override
     public int getMaxBorrowDays() {
-        return 7;  
+        return 7;
     }
-    
+
     @Override
     public double calculateLateFee(int daysOverdue) {
-        return daysOverdue * 0.50;  
+        return daysOverdue * 0.50;
     }
    
+
     public String getIssueDate() {
         return issueDate;
     }
@@ -56,35 +65,46 @@ public class Periodical extends LibraryItem implements Reservable{
         return volume;
     }
 
-    public String getPublicationDate() {
-        return publicationDate;
-    }
-
     public int getIssueNumber() {
         return issueNumber;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
     }
 
     @Override
     public List<String> getSearchableFields() {
         List<String> fields = new ArrayList<>();
-        fields.add(this.title);
-        fields.add(this.publisher);
-        fields.add(this.issn);
+        fields.add(getTitle());
+        fields.add(publisher);
+        fields.add(issn);
         return fields;
     }
 
     @Override
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    @Override
     public void reserve(LibraryMember libraryMember) {
+        if (isReserved) {
+            throw new Error("Periodical is already reserved.");
+        }
         this.isReserved = true;
+        this.reservedBy = libraryMember;
     }
 
     @Override
     public void cancelReserve(LibraryMember libraryMember) {
+        if (!isReserved) {
+            throw new Error("Periodical is not currently reserved.");
+        }
+        if (!reservedBy.equals(libraryMember)) {
+            throw new Error("This periodical was reserved by another member.");
+        }
         this.isReserved = false;
-    }
-
-    @Override
-    public boolean isReserved() {
-        return this.isReserved;
+        this.reservedBy = null;
     }
 }
